a70bb3cb9efa0537a466a3441065c9d4e1d88c32
Your Name <you@example.com>
2019-11-10 00:02:55 +0700

    using static file and create menu navbar bootstrap

--- bai6-lowdb/index.js
+++ bai6-lowdb/index.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 3001;
 const userRoute = require('./routes/user.route')
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index', {name: "Lê Văn Thảo"}));
 
