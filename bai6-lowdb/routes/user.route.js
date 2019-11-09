ecfe28fce7be6f8e51db365e1202b901f8d4fa0a
Your Name <you@example.com>
2019-11-09 13:29:30 +0700

    Delete some redundancy in routes file

--- bai6-lowdb/routes/user.route.js
+++ bai6-lowdb/routes/user.route.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user.controller')
-
-const db = require('../db');
+const userController = require('../controllers/user.controller');
 
 router.get('/', userController.index);
 
