8585fde6dce0ed9f699850cc0706ffd707f51357
Your Name <you@example.com>
2019-11-11 16:18:05 +0700

    User validation

--- bai6-lowdb/controllers/user.controller.js
+++ bai6-lowdb/controllers/user.controller.js
@@ -13,6 +13,20 @@ const search = (req, res) => {
 
 const postCreate =  (req, res) => {
     // let data = req.body;
+    let errors = [];
+    if (!req.body.name) {
+        errors.push("Name is required");
+    }
+    if (!req.body.age) {
+        errors.push("Age is required");
+    }
+    if (!req.body.phone) {
+        errors.push("Phone is required");
+    }
+    if (errors.length) {
+        res.render('users/create', { errors, values: req.body });
+        return;
+    }
     let data = {
         id: shortid.generate(),
         name: req.body.name,
