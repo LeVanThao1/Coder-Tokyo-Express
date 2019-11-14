2c665638927ca2e1b7209c54754cb670474dc780
Your Name <you@example.com>
2019-11-14 20:40:50 +0700

    Environment Variables using dotenv npm

--- bai6-lowdb/index.js
+++ bai6-lowdb/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3001;
@@ -9,7 +10,7 @@ const authRoute = require('./routes/auth.route');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(cookieParser('tiwoqunsady1414klnas'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
